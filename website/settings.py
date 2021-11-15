33a4876ef11c127a9b60fe07cd6a9d4c4e73e0c0
martin machua <machuamartin@gmail.com>
2021-11-15 12:23:58 +0300

    Updated Settings2

--- website/settings.py
+++ website/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b069e=i2&jweo9*z!5cjnyad=#reh5^x_l!@#mu!#kciso7$#s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["besttechproducts.herokuapp.com"]
 
 
 # Application definition
